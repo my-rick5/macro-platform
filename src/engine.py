6e8874c2d461982092800505dfde2fd96097be40
Zachary Myrick <zach.myrick@fullmeasureed.com>
2026-02-14 17:03:58 -0500

    testing

--- src/engine.py
+++ src/engine.py
@@ -58,7 +58,7 @@ print("✅ Artifacts saved to ./results/")
 
 
 # Select the variables you actually care about
-vars_to_plot = ['rff', 'lur', 'pcepie', 'xgdp']
+vars_to_plot = ['rff', 'lur', 'pcepi', 'xgdp']
 fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 axes = axes.flatten()
 
