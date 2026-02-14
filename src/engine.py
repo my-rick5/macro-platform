dc7d5014423b104ceebeb0fa353673f1197d29a8
Zachary Myrick <zach.myrick@fullmeasureed.com>
2026-02-14 14:55:28 -0500

    testing

--- src/engine.py
+++ src/engine.py
@@ -12,27 +12,28 @@ data = load_data("data/LONGBASE.TXT")
 # Load model
 frbus = Frbus("pyfrbus/models/model.xml")
 
-# Specify dates
-start = pandas.Period("2040Q1")
-end = start + 23
+# Specify dates and other params
+residstart = "1975q1"
+residend = "2018q4"
+simstart = "2040q1"
+simend = "2045q4"
+# Number of replications
+nrepl = 1000
+# Run up to 5 extra replications, in case of failures
+nextra = 5
 
-# Standard configuration, use surplus ratio targeting
-data.loc[start:end, "dfpdbt"] = 0
-data.loc[start:end, "dfpsrp"] = 1
+# Policy settings
+data.loc[simstart:simend, "dfpdbt"] = 0
+data.loc[simstart:simend, "dfpsrp"] = 1
 
-# Solve to baseline with adds
-with_adds = frbus.init_trac(start, end, data)
+# Compute add factors
+# Both for baseline tracking and over history, to be used as shocks
+with_adds = frbus.init_trac(residstart, simend, data)
 
-# 100 bp monetary policy shock and solve
-with_adds.loc[start, "rffintay_aerr"] += 1
-sim = frbus.solve(start, end, with_adds)
+# Call FRBUS stochsim procedure
+solutions = frbus.stochsim(
+    nrepl, with_adds, simstart, simend, residstart, residend, nextra=nextra
+)
 
-# View results
-sim_plot(with_adds, sim, start, end)
-
-# Archive data
-sim.to_csv("results/sim_results.csv")
-
-# Archive plot
-sim_plot(with_adds, sim, start, end)
-plt.savefig("results/sim_plot.png")
+stochsim_plot(with_adds, solutions, simstart, simend)
+plt.savefig(results/stochsim_plot.png)
